ec575a40deea058b8f50669dab588c88ddbdc00e
swarnimsaurabh <Swarnim.saurabh@neosoftmail.com>
2026-07-02 15:23:46 +0530

    .

--- WebApplication5/Controllers/WeatherForecastController.cs
+++ WebApplication5/Controllers/WeatherForecastController.cs
@@ -21,6 +21,7 @@ namespace WebApplication5.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            //.
             return Enumerable.Range(1, 3).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
